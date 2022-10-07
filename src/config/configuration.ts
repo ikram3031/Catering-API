030eb05879afff40f371f44331e455d10df4b42c
ikram3031 <mdikram.3031@gmail.com>
2022-10-07 11:22:46 +0600

    updated

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,7 +2,6 @@ export default () => ({
   productionBuild: process.env.PRODUCTION_BUILD === 'true',
   hostname: `http://localhost:${process.env.PORT || 3000}`,
   port: parseInt(process.env.PORT, 10) || 3000,
-  // mongoCluster: `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@localhost:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=${process.env.AUTH_SOURCE}`,
   mongoCluster: `mongodb://localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   userJwtSecret: process.env.JWT_PRIVATE_KEY_USER,
   adminJwtSecret: process.env.JWT_PRIVATE_KEY_ADMIN,
